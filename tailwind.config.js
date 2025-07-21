f40e7a13549306b6a8ee2457b45a7b7e76dfceb2
정준영 <wn8624@naver.com>
2025-07-21 23:43:29 +0900

    chore: App.css + tailwind.config.js 설정

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+const config = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        black: "var(--color-black)",
+        white: "var(--color-white)",
+        gray: {
+          100: "var(--color-gray-100)",
+          300: "var(--color-gray-300)",
+          500: "var(--color-gray-500)",
+          800: "var(--color-gray-800)",
+        },
+      },
+      fontSize: {
+        xs: ["var(--font-size-xs)", { lineHeight: "var(--line-height-20)" }],
+        sm: ["var(--font-size-sm)", { lineHeight: "var(--line-height-22)" }],
+        md: ["var(--font-size-md)", { lineHeight: "var(--line-height-24)" }],
+        lg: ["var(--font-size-lg)", { lineHeight: "var(--line-height-26)" }],
+        "2lg": ["var(--font-size-2lg)", { lineHeight: "var(--line-height-26)" }],
+        xl: ["var(--font-size-xl)", { lineHeight: "var(--line-height-32)" }],
+        "2xl": ["var(--font-size-2xl)", { lineHeight: "var(--line-height-32)" }],
+        "3xl": ["var(--font-size-3xl)", { lineHeight: "var(--line-height-42)" }],
+      },
+      fontWeight: {
+        regular: "var(--font-weight-regular)",
+        medium: "var(--font-weight-medium)",
+        semibold: "var(--font-weight-semibold)",
+        bold: "var(--font-weight-bold)",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
